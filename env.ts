151524f2e4e395d65c6bd2f0ff9b323b6f344324
Kaykys468 <119024696+Kaykys468@users.noreply.github.com>
2024-12-25 20:35:56 -0300

    Update env.ts

--- env.ts
+++ env.ts
@@ -4,5 +4,5 @@ import { cleanEnv, str } from "envalid";
 await config({ export: true });
 
 export default cleanEnv(Deno.env.toObject(), {
-  BOT_TOKEN: str(),
+  BOT_TOKEN = "7529109508:AAG2qcENKveNMskyy4f0H-0S0DLaDxBRmdY"; str(),
 });
